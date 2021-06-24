f486768a660f0385452fe866a0eae6a69458924e
Scoefield <scoefield@163.com>
2021-06-24 20:06:55 +0800

    update linkReve

--- solution/link/linkRevese/code/singleLinkReve.go
+++ solution/link/linkRevese/code/singleLinkReve.go
@@ -1,27 +1,26 @@
 package main
 
-// 定义节点结构体
-type Node struct {
-	Data interface{} // 接口类型的数据data
-	Next *Node       // 下一个节点
-}
-
-// 定义单向链表结构体
-type LinkList struct {
-	Header *Node // 头节点，指向第一个节点
-	Length int   // 链表长度
-}
+import (
+	. "GitCode/leetcode/solution/link/comm"
+)
 
 // 单向链表翻转
-func (l *LinkList) ReverseList() {
-	current := l.Header
+func MyReverseList(head *Node) *Node {
+	current := head
 	var pre *Node
 	for current != nil {
 		current, current.Next, pre = current.Next, pre, current
 	}
-	l.Header = pre
+	return pre
 }
 
 func main() {
+	link := CreateLinkList()
+	link.Append(1)
+	link.Append(2)
+	link.Append(3)
+	link.Append(4)
 
+	node := MyReverseList(link.Header)
+	PrintNodeByHead(node)	
 }
--- solution/testdemo/testdem.go
+++ solution/testdemo/testdem.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	. "GitCode/leetcode/solution/link/comm"
+	// "fmt"
 )
 
 func demo1(head *Node) *Node {
@@ -16,14 +17,58 @@ func demo1(head *Node) *Node {
 	return dumy.Next
 }
 
+func demo2(head *Node) []int {
+	ret := make([]int, 0)
+	for head != nil {
+		ret = append([]int{head.Data}, ret...)
+		head = head.Next
+	}
+	return ret
+}
+
+func demo3(head *Node) *Node {
+	cur := head
+	pre := &Node{}
+	for cur != nil {
+		cur, cur.Next, pre = cur.Next, pre, cur
+	}
+	return pre
+}
+
+func demo4(l1, l2 *Node) *Node {
+	head := &Node{}
+	cur := head
+	for l1 != nil && l2 != nil {
+		if l1.Data < l2.Data {
+			cur.Next = l1
+			l1 = l1.Next
+		} else {
+			cur.Next = l2
+			l2 = l2.Next
+		}
+		cur = cur.Next
+	}
+	
+	if l1 == nil {
+		cur.Next = l2
+	} else if l2 == nil {
+		cur.Next = l1
+	}
+
+	return head.Next
+}
+
 func main() {
 	link := CreateLinkList()
-	link.Append(20)
-	link.Append(21)
-	link.Append(22)
-	link.Append(23)
-	link.Append(24)
+	link.Append(1)
+	link.Append(2)
+	link.Append(4)
 
-	dumy := demo1(link.Header)
-	PrintNodeByHead(dumy)
+	link2 := CreateLinkList()
+	link2.Append(1)
+	link2.Append(3)
+	link2.Append(4)
+
+	mergeHeadNode := demo4(link.Header, link2.Header)
+	PrintNodeByHead(mergeHeadNode)
 }
